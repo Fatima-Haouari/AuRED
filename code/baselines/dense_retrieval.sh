020850b31955c72268be75b2d477ae36eeacebae
Fatima Haouari <71061623+Fatima-Haouari@users.noreply.github.com>
2024-02-08 12:49:40 +0300

    Delete code directory

--- code/baselines/dense_retrieval.sh
+++
@@ -1,3 +0,0 @@
-python dense_retrieval.py --infile /data/fatima/evidence-retrieval/SIGIR2024/data/release/AuRED_test.json \
---outfile ./output/mcontriever-msmarco_test.txt \
---model "facebook/mcontriever-msmarco" 
